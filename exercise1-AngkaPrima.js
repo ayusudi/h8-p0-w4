0fce5c3d3b789fbf083dff91a27d92f80bc104b3
ayusudi <ayusudi.abc@gmail.com>
2019-06-11 21:13:22 +0700

    revisi

--- exercise1-AngkaPrima.js
+++ exercise1-AngkaPrima.js
@@ -1,21 +1,15 @@
 function angkaPrima(angka) {
+  var akarAngka = Math.sqrt(angka);
+  var cekPangkat = Math.trunc(akarAngka);
   var result = true;
-  if (angka === 2) {
-    result = true;
-  } else if (angka === 3) {
-    result = true;
-  } else if (angka === 5) {
-    result = true;
-  } else if (
-    angka % 2 !== 0 &&
-    angka % 3 !== 0 &&
-    angka % 5 !== 0 &&
-    angka / angka == 1
-  ) {
-    result = true;
-  } else {
+  if (akarAngka === cekPangkat) {
     result = false;
+  } else if (angka % 2 === 0 && angka !== 2) {
+    result = false;
+  } else {
+    result = true;
   }
+
   return result;
 }
 
@@ -24,6 +18,3 @@ console.log(angkaPrima(3)); // true
 console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
-console.log(angkaPrima(33)); // false
-// console.log(angkaPrima(5)); // true
-// console.log(angkaPrima(2)); // true
